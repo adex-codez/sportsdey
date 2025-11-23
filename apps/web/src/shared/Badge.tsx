46ebe686f1a5d1fa231b8e7780625755f10aa1e5
Hunchojoe19 <enubiakjoseph@gmail.com>
2025-11-23 04:26:29 +0100

    updated the height to be 12

--- apps/web/src/shared/Badge.tsx
+++ apps/web/src/shared/Badge.tsx
@@ -19,7 +19,7 @@ const Badge = ({label, totalNumber}: BadgeProps) => {
   return (
 
 
-        <button onClick={handleFilterChange} title='filter' type='button' className={`cursor-pointer w-max px-4 capitalize flex justify-center gap-x-2 h-14 text-xs items-center rounded-2xl ${activeFilter === label ? "bg-[#4BA53D] text-white": "bg-white text-[#040C01]"}`}>
+        <button onClick={handleFilterChange} title='filter' type='button' className={`cursor-pointer w-max px-4 capitalize flex justify-center gap-x-2 h-12 text-xs items-center rounded-2xl ${activeFilter === label ? "bg-[#4BA53D] text-white": "bg-white text-[#040C01]"}`}>
             {label}
             <span className={`w-6 text-[9px] flex justify-center items-center h-6 text-white rounded-full ${activeFilter === label ? "bg-[#456041]": "bg-[#040C01]"}`}>{totalNumber}</span>
         </button>
