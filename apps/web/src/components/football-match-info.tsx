c7f92e8a23fc18ec5c92d76a824645a9a4e70ceb
adnatecodez <adeaduratimilehin@gmail.com>
2026-01-05 16:21:29 +0100

    fixed standings table for football

--- apps/web/src/components/football-match-info.tsx
+++ apps/web/src/components/football-match-info.tsx
@@ -11,36 +11,38 @@ export const FootballMatchInfo = ({
 	return (
 		<div className="space-y-4">
 			<div className="rounded-2xl bg-white">
-				<div className="grid grid-cols-[1.2rem_minmax(0,1fr)_repeat(6,4rem)] px-4 py-2 font-semibold">
-					<p />
-					<p className="truncate">Standings</p>
-					<p className="text-center">P</p>
-					<p className="text-center">W</p>
-					<p className="text-center">D</p>
-					<p className="text-center">L</p>
-					<p className="text-center">GD</p>
-					<p className="text-center">PTS</p>
+				<div className="flex justify-between items-center px-4 py-2 font-semibold">
+					<div className="w-full"><span className="text-primary font-semibold text-sm truncate">Standings</span></div>
+					<div className="flex gap-x-4">
+<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">P</span>
+<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">W</span>
+<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">D</span>
+<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">L</span>
+<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">GD</span>
+<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">PTS</span>
+					</div>
+					
 				</div>
 
 				{info.standings?.map((standing) => (
 					<div
 						key={standing.id}
-						className="grid grid-cols-[1.2rem_minmax(0,1fr)_repeat(6,4rem)] border-gray-200 border-t px-4 py-2"
+						className="flex justify-center items-center border-gray-200 border-t px-4 py-2"
 					>
-						{/* Position */}
-						<p className="text-gray-600">{standing.position}</p>
-
-						{/* Team name — allow it to shrink */}
-						<p className="min-w-0 overflow-hidden truncate text-primary">
-							{standing.name}
-						</p>
-
-						<p className="text-center">{standing.played}</p>
-						<p className="text-center">{standing.won}</p>
-						<p className="text-center">{standing.drawn}</p>
-						<p className="text-center">{standing.lost}</p>
-						<p className="text-center">{standing.goal_diff}</p>
-						<p className="text-center">{standing.points}</p>
+						<div className="flex gap-6 w-full">
+							<span className="text-gray-600">{standing.position}</span>
+							<span className="min-w-0 overflow-hidden truncate text-primary">
+								{standing.name}
+							</span>
+						</div>
+						<div className="flex gap-x-4">
+							<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">{standing.played}</span>
+							<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">{standing.won}</span>
+							<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">{standing.drawn}</span>
+							<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">{standing.lost}</span>
+							<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">{standing.goal_diff}</span>
+							<span className="text-primary font-semibold text-xs w-4 md:w-10 lg:w-14 text-center">{standing.points}</span>
+						</div>
 					</div>
 				))}
 			</div>
